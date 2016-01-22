ab8fa930d07a426a7e5f4cdbc8b3b50a8501e007
Amanda <amandadelreino@gmail.com>
2016-01-22 09:45:28 +0100

    Este es el commit del proyecto PATO cuac cuac

---
+++ Pato/src/pato/Pato.java
@@ -0,0 +1,24 @@
+package pato;
+
+public class Pato 
+{
+	private int Posicion;
+	
+	public Pato (int pos)
+	{
+		this.Posicion=pos;
+	}
+	
+	public int AndarHaciaDelante(int pasos) 
+	{
+		return this.Posicion+=pasos;
+	}
+	public int AndarHaciaAtras(int pasos) 
+	{
+		return this.Posicion-=pasos;
+	}
+	public int Dimeposicion()
+	{
+		return this.Posicion;
+	}
+}
